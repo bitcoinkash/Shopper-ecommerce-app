86543100c16bc3da2671e8ae46573c0ff542640e
SUHAIL PARRY <Suhail.parry@mail.mcgill.ca>
2023-07-11 12:28:00 -0400

    preloader component added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
-import React, {useState} from 'react'
-import Nav from './nav'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from './nav';
 import Rout from './rout';
-import {BrowserRouter} from 'react-router-dom';
 import Footer from './footer';
 import Productdetail from './productdetail';
-const App = () => {
-  // add to cart
-  const [cart, setCart] = useState([])
-  //product Detail
-  const [close, setClose] = useState(false)
-  const [detail, setDetail] = useState([])
-  //filter product
-  const [product, setProduct] = useState(Productdetail)
-  const searchbtn = (product) => 
-  {
-    const change = Productdetail.filter((x) => 
-    {
-      return x.Cat === product
-    })
-    setProduct(change)
-  }
-  //product detail
-  const view = (product) => 
-  {
-    setDetail([{...product}])
-    setClose(true)
-  }
+import Preloader from './Preloader';
 
-  // add to cart
-  const addtocart = (product) => 
-  {
-    const exsit = cart.find((x) => 
-    {
-      return x.id === product.id
-    })
-    if(exsit)
-    {
-      alert("This Product is already added to cart")
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState([]);
+  const [close, setClose] = useState(false);
+  const [detail, setDetail] = useState([]);
+  const [product, setProduct] = useState(Productdetail);
+
+  useEffect(() => {
+    // Simulate a delay to showcase the preloader
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  const searchbtn = (product) => {
+    const change = Productdetail.filter((x) => {
+      return x.Cat === product;
+    });
+    setProduct(change);
+  };
+
+  const view = (product) => {
+    setDetail([{ ...product }]);
+    setClose(true);
+  };
+
+  const addtocart = (product) => {
+    const exsit = cart.find((x) => {
+      return x.id === product.id;
+    });
+    if (exsit) {
+      alert('This Product is already added to cart');
+    } else {
+      setCart([...cart, { ...product, qty: 1 }]);
+      alert('Product is added to cart');
     }
-    else
-    { 
-      setCart([...cart, {...product, qty:1}])
-      alert("product is added to cart")
-    }
-  } 
-  console.log(cart)
+  };
+
   return (
     <>
-    <BrowserRouter>
-    <Nav searchbtn={searchbtn}/>
-    <Rout product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} cart={cart} setCart={setCart} addtocart={addtocart}/>
-    <Footer />
-    </BrowserRouter>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <BrowserRouter>
+          <Nav searchbtn={searchbtn} />
+          <Rout
+            product={product}
+            setProduct={setProduct}
+            detail={detail}
+            view={view}
+            close={close}
+            setClose={setClose}
+            cart={cart}
+            setCart={setCart}
+            addtocart={addtocart}
+          />
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
